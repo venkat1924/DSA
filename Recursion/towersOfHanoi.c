c56fa8c2233d3d37c4d83075352319d28414664f
Anumaneni Venkat Balachandra <128356330+venkat1924@users.noreply.github.com>
2024-01-07 23:07:14 +0530

    Update towersOfHanoi.c

--- Recursion/towersOfHanoi.c
+++ Recursion/towersOfHanoi.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 
+//s=source rod, d=destination rod, t=temporary rod
 void TowerHanoi(int n, char s, char d, char t)
 {
   if (n==1)
@@ -7,7 +8,7 @@ void TowerHanoi(int n, char s, char d, char t)
   else
   {
     TowerHanoi(n-1,s,t,d);
-    printf("move %d disc from %c to %c\n",n,s,d);
+    printf("Move disc %d from %c to %c.\n",n,s,d);
     TowerHanoi(n-1,t,d,s);
   }
 }
@@ -19,4 +20,4 @@ scanf("%d",&n);
 TowerHanoi(n,'S','D','T');
 return 0;
 }
-//S=source rod, D=destination rod,T=temporary rod
+
