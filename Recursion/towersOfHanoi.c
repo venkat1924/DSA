42e1618ffbaef24620e588bedafa9a0a7101317d
Anumaneni Venkat Balachandra <128356330+venkat1924@users.noreply.github.com>
2024-01-07 23:18:07 +0530

    Update towersOfHanoi.c
    Changed variable names, added comments.

--- Recursion/towersOfHanoi.c
+++ Recursion/towersOfHanoi.c
@@ -1,23 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 
-//s=source rod, d=destination rod, t=temporary rod
-void TowerHanoi(int n, char s, char d, char t)
+void towersOfHanoi (int no_of_discs, char source, char destination, char temporary)
 {
-  if (n==1)
-    printf("move %d disc from %c to %c\n",n,s,d);
-  else
-  {
-    TowerHanoi(n-1,s,t,d);
-    printf("Move disc %d from %c to %c.\n",n,s,d);
-    TowerHanoi(n-1,t,d,s);
-  }
+    if (no_of_discs==1)
+        // Move the one disk from the source to the destination.
+        printf("Move disc %d from %c to %c.\n", no_of_discs, source, destination);
+    
+    else
+    {
+        // Move no_of_discs-1 from the source to the temporary using the destination.
+        towersOfHanoi(no_of_discs-1, source, temporary, destination);
+        printf("Move disc %d from %c to %c.\n", no_of_discs, source, destination);
+        
+        //Move the no_of_discs-1 back from the temporary to the destination using the source.
+        towersOfHanoi(no_of_discs-1, temporary, destination, source);
+    }
 }
+
 int main()
 {
-int n;
-printf("Enter number of disks here:");
-scanf("%d",&n);
-TowerHanoi(n,'S','D','T');
-return 0;
+    int n;
+    
+    printf("Enter the number of disks: ");
+    scanf("%d", &n);
+    
+    towersOfHanoi(n, 'S', 'D', 'T');
+    
+    return 0;
 }
-
